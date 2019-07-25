11c89271d2fb8ac4dc05eb525400a9d34e0314f0
Northisland <shihomi0104@icloud.com>
2019-07-24 19:21:00 -0700

    anyway I finish it, but i need to improve it.

--- ship.java
+++ ship.java
@@ -1,9 +1,6 @@
 package battleShip;
 
-
-import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class ship {
 	
@@ -44,9 +41,12 @@ public class ship {
 		System.out.println();
 		
 		// i is y coordinate.
-		// left and right of the playground.
+		// left and right sides of the playground.
 		for (int y = 0; y < 10; y++) {
+			// left side
 			System.out.print(y);
+			// between left and right
+			// they are status.
 			for (int x = 0; x <= 9; x++) {
 				if( grid[x][y] == 0) {
 				System.out.print(" . "); // nothing
@@ -65,9 +65,10 @@ public class ship {
 					System.out.print(" + "); // enemy miss
 				}
 				else if (grid[x][y] == 6) {
-					System.out.println(" - "); // user miss
+					System.out.print(" - "); // user miss
 				}
 				}
+			// right side 
 			System.out.println("  " + y);
 			
 			}
@@ -88,12 +89,10 @@ public class ship {
 		System.out.println("Here is the playground.");
 	}
 	
+	// at first cp ask user to decide position.
 	public static void userShip() {		
 		System.out.println("Where do you want to deploy your ships?");
 		System.out.println("At first, both you and cp have 5 ships.");
-		
-		//ship myObj = new ship();
-		
 		
 		for ( int k = userNum; k > 0 ; k--) {
 			System.out.printf("NO. %d", k);	
@@ -107,26 +106,29 @@ public class ship {
 		}
 	}
 	
+	// cp determine the position by this code only first time.
 	public static void AIShip() {
 
 		for( int i = AINum; i > 0; i--) {
-//			xAI = (int)(Math.random() * 9);
-//			yAI = (int)(Math.random() * 9);
 			
 			do {
 				xAI = (int)(Math.random() * 9);
 				yAI = (int)(Math.random() * 9);
-					
-			}while( grid[xAI][yAI] == 1);
+			// no to be overlapped with user's ship and the position Aiship is determined.			
+			}while(grid[xAI][yAI] == 1 || grid[xAI][yAI] == 2);
+			
+				
+			 
 
-			//System.out.printf("xAI is %d, yAI is %d", xAI, yAI);
-			//System.out.println();
+			System.out.printf("xAI is %d, yAI is %d", xAI, yAI);
+			System.out.println();
 			
 			grid[xAI][yAI] = 2;
 			
 		}
 	}
 	
+	// second time or later, user follow this code to decide where position it is. 
 	public static void UserCheckShipSunk() {
 		System.out.println("Now your turn!");
 		System.out.print("tell me x coordinate. : ");
@@ -137,61 +139,69 @@ public class ship {
 		
 		if(grid[x][y] == 2) {
 //			コンピューターがいた場合、沈める
+			// which means the cp ship is sunk if it exist there.
 			grid[x][y] = 3;
 			AINum--;
 		}
+		if (grid[x][y] == 0) {
+			grid[x][y] = 6;	
+		}
+		
 	
 	}
-	
+	// second time or later cp folow this code to decide where position it hit.
 	public static void AICheckShipSunk() {
 		
-		//int xAI = (int)(Math.random() * 9);
-		//int yAI = (int)(Math.random() * 9);
 		
 		do {
 			xAI = (int)(Math.random() * 9);
 			yAI = (int)(Math.random() * 9);
-				
-		}while( grid[xAI][yAI] == 4|| grid[xAI][yAI] == 2 || grid[xAI][yAI] == 5);
+	// no to be overlapped with position already determined status.
+		}while( grid[xAI][yAI] == 2|| grid[xAI][yAI] == 3 || grid[xAI][yAI] == 4 || grid[xAI][yAI] == 5 || grid[xAI][yAI] == 6);
 
 		System.out.printf("xAI is %d, yAI is %d", xAI, yAI);
 		System.out.println();
 		
+		// if there is a user ship, kill it
 		if (grid[xAI][yAI] == 1) {
-			grid[x][y] = 4;
+			grid[xAI][yAI] = 4;
 			userNum--;
+		} 
+		// if there is nothing, that means miss.
+		if ( grid[xAI][yAI] == 0) {
+			grid[xAI][yAI] = 5;
 		}
-			grid[xAI][yAI] = 5;	
+				
 	
 		}
 	
 	
 	public static void theNumOfShip() {
+		// how many ships user and cp have?
 		System.out.printf("Now you have %d ships. ", userNum);
 		System.out.printf("CP has %d ships.", AINum);
 		System.out.println();
 		
+		// the number user ships lost
 		if( userNum < 5) {
 			System.out.printf("you lost %d", 5 - userNum);
 			System.out.println();
 			
-			int xAI = (int)(Math.random() * 9);
-			int yAI = (int)(Math.random() * 9);
-			
-//			if ( grid[xAI][yAI] = 1; )
-//			grid[xAI][yAI] = 2;
-			
-			
+			xAI = (int)(Math.random() * 9);
+			yAI = (int)(Math.random() * 9);
 			 
 			System.out.println();
 		}
+		// the number cp ship lost.
 		if(AINum < 5) {
-			System.out.printf(" CP lost %d ", 5 - AINum);
+			System.out.printf("CP lost %d ", 5 - AINum);
 			System.out.println();
 			
 			
 		}
 	}
+	
+
 	
 	public static void main(String[] args) {
 		askFirst();
@@ -203,23 +213,37 @@ public class ship {
 		playground(grid);
 		theNumOfShip();
 		
-		while( userNum > 0 && AINum > 0) {
-			System.out.println("");
-			UserCheckShipSunk();
-			playground(grid);
-			theNumOfShip();
+		
+		// if either user or cp lost, can go out while loop.
+		while(userNum != 0 && AINum != 0){
 			
-			System.out.println("");
-			AICheckShipSunk();		
-			playground(grid);
-			theNumOfShip();
+			// if user ships are gone, it doesn't happen.
+			if( userNum > 0) {
+		
+				System.out.println("");
+				UserCheckShipSunk();
+				playground(grid);
+				theNumOfShip();	
+			}
 			
+			// if cp ships are gone, it doesn't happen.
+			if ( AINum > 0) {
+				
+				System.out.println("");
+				AICheckShipSunk();		
+				playground(grid);
+				theNumOfShip();
+			}	
 		}
+		
+		// when user ships are gone
 		if ( userNum == 0) {
 			System.out.println("you lost!");
 		}
+		
+		// when cp ships are gone
 		if( AINum == 0) {
-			System.out.println("cp lost!");
+			System.out.println("you won!");
 		}
 		
 	}
